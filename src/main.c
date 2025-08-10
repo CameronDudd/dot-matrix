2f9d3ecb410ed7bded835c19715da88fcd81804c
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-08-10 14:22:06 +0100

    feat(main): Hello, world!

--- src/main.c
+++ src/main.c
@@ -1,39 +1,27 @@
-#include <stdint.h>
+/*
+ *   Copyright (c) 2024 Cameron Dudd
+ *   All rights reserved.
+ */
 
 #include "color.h"
+#include "display.h"
 #include "gpio.h"
 #include "time.h"
 
-int main(void) {
-  RGBColor1 _buff[64][64];
-  for (uint8_t row = 0; row < 64; ++row) {
-    for (uint8_t col = 0; col < 64; ++col) {
-      _buff[row][col].r = (col % 2) ? 1 : 0;
-      _buff[row][col].g = ((col % 2) && (row % 2)) ? 1 : 0;
-      _buff[row][col].b = 0;
-    }
-  }
-
+static void init(void) {
   systickInit(16000000);
   gpioInit();
-  sleepMs(1000);
+  clearDisplay();
+}
 
+int main(void) {
+  init();
+  str2display(0, 0, "Hello, world!", RED);
+  str2display(7, 0, "Hello, world!", GREEN);
+  str2display(14, 0, "Hello, world!", BLUE);
+  str2display(21, 0, "Hello, world!", WHITE);
   while (1) {
-    for (uint8_t row = 0; row < 32; ++row) {
-      outputDisable();
-
-      selectRow(row);
-
-      for (uint8_t col = 0; col < 64; ++col) {
-        setColorLines(_buff[row][col], 0);
-        setColorLines(_buff[row + 32][col], 1);
-        clock();
-      }
-
-      latch();
-      outputEnable();
-      sleepMs(1);
-    }
+    renderDisplay();
   }
   return 0;
 }
