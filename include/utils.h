e9c839b3ad17dda98aec01af2e6bb565b651b0f5
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-08-14 01:00:22 +0100

    feat(dot-matrix): add utils

---
+++ include/utils.h
@@ -0,0 +1,13 @@
+/*
+ *   Copyright (c) 2025 Cameron Dudd
+ *   All rights reserved.
+ */
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stdint.h>
+
+void itoa(uint32_t i, char *a);
+
+#endif  // UTILS_H
