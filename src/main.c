93ec329b5bdc0322e10493e3ee5de005baa5a012
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-08-14 01:44:00 +0100

    feat(main): simple usart output

--- src/main.c
+++ src/main.c
@@ -3,19 +3,24 @@
  *   All rights reserved.
  */
 
-#include "gpio.h"
 #include "time.h"
+#include "usart.h"
+#include "utils.h"
+
+#define SYSCLK 16000000UL  // 16MHz HSI
 
 static void init(void) {
-  systickInit(16000000);
-  gpioInit();
+  systickInit(SYSCLK);
+  tim2Init(SYSCLK);
+  usartInit(SYSCLK);
 }
 
 int main(void) {
   init();
+  char buff[200];
   while (1) {
-    gpioToggleLED();
-    sleepMs(1000);
+    itoa(1234, buff);
+    usart2TxStr(buff);
+    sleepUs(1000000);
   }
-  return 0;
 }
