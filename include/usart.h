f34bd57866dc546bafe35d999a3159b7b585a1fb
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-08-18 00:27:12 +0100

    feat(usart): Usart2GlobalInterrupt_Handler

--- include/usart.h
+++ include/usart.h
@@ -8,10 +8,15 @@
 
 #include <stdint.h>
 
-#define BAUD 9600
+#define BAUD           9600
+#define RECV_BUFF_SIZE 685
 
+extern volatile char recvBuffComplete;
+extern volatile char recvBuff[RECV_BUFF_SIZE];
+
+void Usart2GlobalInterrupt_Handler(void);
 void usartInit(uint32_t clkHz);
-void usart2TxChar(const uint8_t c);
+void usart2TxChar(const char c);
 void usart2TxStr(const char *s);
 
 #endif  // USART_H
--- src/usart.c
+++ src/usart.c
@@ -11,6 +11,26 @@
 #define AF_BITS_PER_PIN 4
 #define AF_USART        7u
 
+static volatile char _c;
+volatile char recvBuffComplete = 0;
+volatile char recvBuff[RECV_BUFF_SIZE];
+volatile char *_recvBuffPtr = recvBuff;
+
+void Usart2GlobalInterrupt_Handler(void) {
+  if (USART2->SR & USART_SR_RXNE) {
+    _c = USART2->DR;
+    if ((_recvBuffPtr - recvBuff) >= RECV_BUFF_SIZE) {
+      recvBuffComplete = 0;
+      _recvBuffPtr     = recvBuff;
+    } else if (_c == '@') {
+      _recvBuffPtr     = recvBuff;
+      recvBuffComplete = 1;
+    } else {
+      *_recvBuffPtr++ = _c;
+    }
+  }
+}
+
 void usartInit(uint32_t clkHz) {
   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
   RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
@@ -26,12 +46,18 @@ void usartInit(uint32_t clkHz) {
   GPIOA->AFR[0] |= (AF_USART << (2 * AF_BITS_PER_PIN)) | (AF_USART << (3 * AF_BITS_PER_PIN));
 
   USART2->BRR = clkHz / BAUD;
-  USART2->CR1 &= ~USART_CR1_M;                                 // word Length: 1 Start bit, 8 Data bits, n Stop bit
-  USART2->CR2 &= ~USART_CR2_STOP_Msk;                          // 00: 1 Stop bit
-  USART2->CR1 = (USART_CR1_TE | USART_CR1_RE | USART_CR1_UE);  // enable tx, rx, usart
+  USART2->CR1 &= ~USART_CR1_M;                                                    // word Length: 1 Start bit, 8 Data bits, n Stop bit
+  USART2->CR2 &= ~USART_CR2_STOP_Msk;                                             // 00: 1 Stop bit
+  USART2->CR1 = (USART_CR1_TE | USART_CR1_RE | USART_CR1_UE | USART_CR1_RXNEIE);  // enable tx, rx, usart, interrupt
+
+  // Clear recv buffer
+  for (int b = 0; b < RECV_BUFF_SIZE; ++b) {
+    recvBuff[b] = '\0';
+  }
+  NVIC_EnableIRQ(USART2_IRQn);  // Enable interrupt
 }
 
-void usart2TxChar(const uint8_t c) {
+void usart2TxChar(const char c) {
   while (!(USART2->SR & USART_SR_TXE)) {
   }
   USART2->DR = c;
