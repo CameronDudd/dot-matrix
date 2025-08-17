57ee6c9335fc71c10d87f02d2d2bcf7531aaa7b2
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-08-18 00:38:39 +0100

    feat(main): display from usart buffer

--- src/main.c
+++ src/main.c
@@ -3,9 +3,9 @@
  *   All rights reserved.
  */
 
-#include "color.h"
 #include "display.h"
 #include "time.h"
+#include "usart.h"
 
 #define SYSCLK 16000000UL  // 16MHz HSI
 #define START  1755177805UL
@@ -13,17 +13,18 @@
 static void init(void) {
   systickInit(SYSCLK);
   tim2Init(SYSCLK);
+  usartInit(SYSCLK);
   displayInit();
 }
 
 int main(void) {
   init();
   while (1) {
-    int2display(0, 0, 1755188300 + epochS(), RED);
-    str2display(19, 22, "LIVE", RED);
-    str2display(26, 22, "LAUGH", RED);
-    str2display(33, 22, "TBONE", RED);
+    if (recvBuffComplete) {
+      clearDisplay();
+      recvBuff2display();
+      recvBuffComplete = 0;
+    }
     renderDisplay();
-    clearDisplay();
   }
 }
