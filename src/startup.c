d630c7104da81a288ccf268c17f45e38da12853d
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-08-18 00:32:45 +0100

    feat(startup): add Usart2GlobalInterrupt_Handler

--- src/startup.c
+++ src/startup.c
@@ -5,7 +5,12 @@
 
 #include <stdint.h>
 
+#include "stm32f401xe.h"
 #include "time.h"
+#include "usart.h"
+
+#define IRQ_ZERO      16
+#define IRQ_POS(irqn) ((irqn) + IRQ_ZERO)
 
 extern uint32_t _stack;
 extern uint32_t _data_loadaddr, _data, _edata;
@@ -14,12 +19,15 @@ extern uint32_t _bss, _ebss;
 void reset_handler(void);
 void default_handler(void);
 
+// clang-format off
 __attribute__((section(".vectors"))) void (*const vector_table[])(void) = {
-    (void (*)(void))(&_stack),
-    reset_handler,
-    default_handler,
-    [15] = SysTick_Handler,
+  (void (*)(void))(&_stack),
+  reset_handler,
+  [2 ... 14] = default_handler,
+  [15] = SysTick_Handler,
+  [IRQ_POS(USART2_IRQn)] = Usart2GlobalInterrupt_Handler,
 };
+// clang-format on
 
 void reset_handler(void) {
   uint32_t *src = &_data_loadaddr;
