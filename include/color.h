0a8b7279cabfa16506f2cd456d2707e8256fae4a
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-09-09 23:25:43 +0100

    feat(color): more color defs
    only support 8bit per channel

--- include/color.h
+++ include/color.h
@@ -9,26 +9,18 @@
 #include <stdint.h>
 
 typedef struct {
-  uint8_t r : 1;
-  uint8_t g : 1;
-  uint8_t b : 1;
-  uint8_t _ : 5;
-} RGBColor1;
-
-typedef struct {
   uint8_t r;
   uint8_t g;
   uint8_t b;
 } RGBColor;
 
-extern const RGBColor1 *WHITE1;
-extern const RGBColor1 *RED1;
-extern const RGBColor1 *GREEN1;
-extern const RGBColor1 *BLUE1;
-
-extern const RGBColor *WHITE;
+extern const RGBColor *BLACK;
 extern const RGBColor *RED;
 extern const RGBColor *GREEN;
+extern const RGBColor *YELLOW;
 extern const RGBColor *BLUE;
+extern const RGBColor *MAGENTA;
+extern const RGBColor *CYAN;
+extern const RGBColor *WHITE;
 
 #endif  // COLOR_H
--- src/color.c
+++ src/color.c
@@ -5,57 +5,59 @@
 
 #include "color.h"
 
-static const RGBColor1 _WHITE1 = {
-    .r = 1,
-    .g = 1,
-    .b = 1,
-};
-
-static const RGBColor1 _RED1 = {
-    .r = 1,
+static const RGBColor _BLACK = {
+    .r = 0,
     .g = 0,
     .b = 0,
 };
 
-static const RGBColor1 _GREEN1 = {
-    .r = 0,
-    .g = 1,
-    .b = 0,
-};
-
-static const RGBColor1 _BLUE1 = {
-    .r = 0,
-    .g = 0,
-    .b = 1,
-};
-
-const RGBColor1 *WHITE1 = &_WHITE1;
-const RGBColor1 *RED1   = &_RED1;
-const RGBColor1 *GREEN1 = &_GREEN1;
-const RGBColor1 *BLUE1  = &_BLUE1;
-
-static const RGBColor _WHITE = {
-    .r = 255,
-    .g = 255,
-    .b = 255,
-};
 static const RGBColor _RED = {
     .r = 255,
     .g = 0,
     .b = 0,
 };
+
 static const RGBColor _GREEN = {
     .r = 0,
     .g = 255,
     .b = 0,
 };
+
+static const RGBColor _YELLOW = {
+    .r = 255,
+    .g = 255,
+    .b = 0,
+};
+
 static const RGBColor _BLUE = {
     .r = 0,
     .g = 0,
     .b = 255,
 };
 
-const RGBColor *WHITE = &_WHITE;
-const RGBColor *RED   = &_RED;
-const RGBColor *GREEN = &_GREEN;
-const RGBColor *BLUE  = &_BLUE;
+static const RGBColor _MAGENTA = {
+    .r = 255,
+    .g = 0,
+    .b = 255,
+};
+
+static const RGBColor _CYAN = {
+    .r = 0,
+    .g = 255,
+    .b = 255,
+};
+
+static const RGBColor _WHITE = {
+    .r = 255,
+    .g = 255,
+    .b = 255,
+};
+
+const RGBColor *BLACK   = &_BLACK;
+const RGBColor *RED     = &_RED;
+const RGBColor *GREEN   = &_GREEN;
+const RGBColor *YELLOW  = &_YELLOW;
+const RGBColor *BLUE    = &_BLUE;
+const RGBColor *MAGENTA = &_MAGENTA;
+const RGBColor *CYAN    = &_CYAN;
+const RGBColor *WHITE   = &_WHITE;
