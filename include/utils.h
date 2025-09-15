a2f8fdfeeaaf5587ae6a21e6c78ebfda4c1a3372
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-09-15 22:18:03 +0100

    feat(utils): memset

--- include/utils.h
+++ include/utils.h
@@ -8,6 +8,7 @@
 
 #include <stddef.h>
 
+void *memset(void *dst, int c, size_t n) __attribute__((weak));
 void *memcpy(void *dst, const void *src, size_t n) __attribute__((weak));
 
 #endif  // UTILS_H
--- src/utils.c
+++ src/utils.c
@@ -5,6 +5,16 @@
 
 #include "utils.h"
 
+__attribute__((weak)) void *memset(void *dst, int c, size_t n) {
+  char *dstPtr = (char *)dst;
+  if (dstPtr) {
+    while (n--) {
+      *dstPtr++ = c;
+    }
+  }
+  return dst;
+}
+
 __attribute__((weak)) void *memcpy(void *dst, const void *src, size_t n) {
   char *dstPtr       = (char *)dst;
   const char *srcPtr = (const char *)src;
