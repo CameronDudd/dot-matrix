5a861ac0118ab32c4e6a61b21c45a17857821e7a
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-09-21 14:45:33 +0100

    feat(rand): better random init

--- src/rand.c
+++ src/rand.c
@@ -23,10 +23,14 @@ void randInit(void) {
   RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;  // Enable ADC1 clock
   ADC1->CR2 |= ADC_CR2_ADON;           // Enable ADC
   ADC1->SQR3 = 0;                      // Channel 0
-  ADC1->CR2 |= ADC_CR2_SWSTART;        // Start conversion
-  while (!(ADC1->SR & ADC_SR_EOC)) {   // While the end of conversion bit not set
+  uint32_t s = 0;
+  for (int i = 0; i < 8; i++) {
+    ADC1->CR2 |= ADC_CR2_SWSTART;       // Start conversion
+    while (!(ADC1->SR & ADC_SR_EOC)) {  // While the end of conversion bit not set
+    }
+    s ^= ADC1->DR << (i * 4);
   }
-  seed = ADC1->DR;  // Read data register
+  seed = s ^ SysTick->VAL;
 }
 
 uint32_t rand(void) {
@@ -34,4 +38,6 @@ uint32_t rand(void) {
   return seed;
 }
 
-int randRange(int min, int max) { return ((int64_t)((int64_t)rand() * (int64_t)(max - min + 1)) >> 32) + min; }
+int randRange(int min, int max) {
+  return ((int64_t)((int64_t)rand() * (int64_t)(max - min + 1)) >> 32) + min;
+}
