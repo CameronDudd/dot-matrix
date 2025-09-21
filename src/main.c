0f6c02b4e7b1b0593de6a80eabd210fa7065205b
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-09-21 23:47:43 +0100

    feat(main): spinning cube

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 
 #include "animation.h"
 #include "display.h"
+#include "geometry.h"
 #include "rand.h"
 #include "renderer.h"
 #include "time.h"
@@ -25,12 +26,16 @@ static void init(void) {
 
 int main(void) {
   init();
+  Mesh3D cube       = cuboidMesh(45, -13, 0, 30, 30, 30);
   int lastUpdatedTs = 0;
   while (1) {
-    if ((lastUpdatedTs == 0) || ((epochMs() - lastUpdatedTs) >= 10)) {
+    if ((lastUpdatedTs == 0) || ((epochMs() - lastUpdatedTs) >= 30)) {
       lastUpdatedTs = epochMs();
-      drawAnimationBuffer();
-      updateAnimation();
+      rollMesh3D(&cube, 1);
+      pitchMesh3D(&cube, 1);
+      yawMesh3D(&cube, 1);
+      clearFrameBuffer();
+      drawMesh3DIsometric(&cube);
     }
     renderDisplay();
   }
