92e3aa5c556cc8a9883e80fd6d8e7f868a983426
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-09-21 22:06:05 +0100

    feat(dot-matrix): add math module

---
+++ include/math.h
@@ -0,0 +1,14 @@
+/*
+ *   Copyright (c) 2025 Cameron Dudd
+ *   All rights reserved.
+ */
+
+#ifndef MATH_H
+#define MATH_H
+
+#define TRIG_LOOKUP_SIZE 360
+
+float sin(float theta);
+float cos(float theta);
+
+#endif  // MATH_H
