6aced2773ffa7b32579d08ceba5b526c974fc255
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-09-22 22:23:31 +0100

    feat(math): add PI

--- include/math.h
+++ include/math.h
@@ -6,6 +6,7 @@
 #ifndef MATH_H
 #define MATH_H
 
+#define PI               (float)3.14159265359
 #define SQRT2            (float)1.41421356237
 #define SQRT3            (float)1.73205080757
 #define RECIPROCAL_SQRT6 (float)0.40824829046
