3b6bab4acc114ecdc46d2e103b9fd830465b9fdb
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-09-26 20:47:03 +0100

    refactor(vec): use pointers to vecs

--- src/vec.c
+++ src/vec.c
@@ -5,43 +5,43 @@
 
 #include "vec.h"
 
-Vec2 add2(Vec2 a, Vec2 b) {  // a + b
+Vec2 add2(Vec2 *a, Vec2 *b) {  // a + b
   return (Vec2){
-      .x = a.x + b.x,
-      .y = a.y + b.y,
+      .x = a->x + b->x,
+      .y = a->y + b->y,
   };
 }
 
-Vec2 sub2(Vec2 a, Vec2 b) {  // a - b
+Vec2 sub2(Vec2 *a, Vec2 *b) {  // a - b
   return (Vec2){
-      .x = a.x - b.x,
-      .y = a.y - b.y,
+      .x = a->x - b->x,
+      .y = a->y - b->y,
   };
 }
 
-float dot2(Vec2 a, Vec2 b) {
-  return (a.x * b.x) + (a.y * b.y);
+float dot2(Vec2 *a, Vec2 *b) {
+  return (a->x * b->x) + (a->y * b->y);
 }
 
-float cross2(Vec2 a, Vec2 b) {
-  return (a.x * b.y) - (a.y * b.x);
+float cross2(Vec2 *a, Vec2 *b) {
+  return (a->x * b->y) - (a->y * b->x);
 }
 
-Vec2 perpendicular2(Vec2 vec) {  // Counter-Clockwise
+Vec2 perpendicular2(Vec2 *vec) {  // Counter-Clockwise
   return (Vec2){
-      .x = -vec.y,
-      .y = vec.x,
+      .x = -vec->y,
+      .y = vec->x,
   };
 }
 
-float dot3(Vec3 a, Vec3 b) {
-  return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
+float dot3(Vec3 *a, Vec3 *b) {
+  return (a->x * b->x) + (a->y * b->y) + (a->z * b->z);
 }
 
-Vec3 cross3(Vec3 a, Vec3 b) {
+Vec3 cross3(Vec3 *a, Vec3 *b) {
   return (Vec3){
-      .x = (a.y * b.z) - (a.z * b.y),
-      .y = (a.z * b.x) - (a.x * b.z),
-      .z = (a.x * b.y) - (a.y * b.x),
+      .x = (a->y * b->z) - (a->z * b->y),
+      .y = (a->z * b->x) - (a->x * b->z),
+      .z = (a->x * b->y) - (a->y * b->x),
   };
 }
