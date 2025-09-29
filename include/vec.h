9392f3393d40d82be9fc8312d18eb8b3c7831ed6
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-09-29 21:05:51 +0100

    feat(vec): by value not by reference

--- include/vec.h
+++ include/vec.h
@@ -17,13 +17,14 @@ typedef struct {
   float z;
 } Vec3;
 
-Vec2 add2(Vec2 *a, Vec2 *b);
-Vec2 sub2(Vec2 *a, Vec2 *b);
-float dot2(Vec2 *a, Vec2 *b);
-float cross2(Vec2 *a, Vec2 *b);
-Vec2 perpendicular2(Vec2 *vec);
+Vec2 add2(Vec2 a, Vec2 b);
+Vec2 sub2(Vec2 a, Vec2 b);
+float dot2(Vec2 a, Vec2 b);
+float cross2(Vec2 a, Vec2 b);
+Vec2 perpendicular2(Vec2 vec);
+int pointOnLeftOfLine(Vec2 a, Vec2 b, Vec2 p);
 
-float dot3(Vec3 *a, Vec3 *b);
-Vec3 cross3(Vec3 *a, Vec3 *b);
+float dot3(Vec3 a, Vec3 b);
+Vec3 cross3(Vec3 a, Vec3 b);
 
 #endif  // VEC_H
--- src/vec.c
+++ src/vec.c
@@ -5,43 +5,50 @@
 
 #include "vec.h"
 
-Vec2 add2(Vec2 *a, Vec2 *b) {  // a + b
+Vec2 add2(Vec2 a, Vec2 b) {  // a + b
   return (Vec2){
-      .x = a->x + b->x,
-      .y = a->y + b->y,
+      .x = a.x + b.x,
+      .y = a.y + b.y,
   };
 }
 
-Vec2 sub2(Vec2 *a, Vec2 *b) {  // a - b
+Vec2 sub2(Vec2 a, Vec2 b) {  // a - b
   return (Vec2){
-      .x = a->x - b->x,
-      .y = a->y - b->y,
+      .x = a.x - b.x,
+      .y = a.y - b.y,
   };
 }
 
-float dot2(Vec2 *a, Vec2 *b) {
-  return (a->x * b->x) + (a->y * b->y);
+float dot2(Vec2 a, Vec2 b) {
+  return (a.x * b.x) + (a.y * b.y);
 }
 
-float cross2(Vec2 *a, Vec2 *b) {
-  return (a->x * b->y) - (a->y * b->x);
+float cross2(Vec2 a, Vec2 b) {
+  return (a.x * b.y) - (a.y * b.x);
 }
 
-Vec2 perpendicular2(Vec2 *vec) {  // Counter-Clockwise
+Vec2 perpendicular2(Vec2 vec) {  // Counter-Clockwise
   return (Vec2){
-      .x = -vec->y,
-      .y = vec->x,
+      .x = -vec.y,
+      .y = vec.x,
   };
 }
 
-float dot3(Vec3 *a, Vec3 *b) {
-  return (a->x * b->x) + (a->y * b->y) + (a->z * b->z);
+int pointOnLeftOfLine(Vec2 a, Vec2 b, Vec2 p) {
+  Vec2 ap     = sub2(p, a);
+  Vec2 ab     = sub2(b, a);
+  Vec2 abPerp = perpendicular2(ab);
+  return dot2(ap, abPerp) >= 0;
 }
 
-Vec3 cross3(Vec3 *a, Vec3 *b) {
+float dot3(Vec3 a, Vec3 b) {
+  return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
+}
+
+Vec3 cross3(Vec3 a, Vec3 b) {
   return (Vec3){
-      .x = (a->y * b->z) - (a->z * b->y),
-      .y = (a->z * b->x) - (a->x * b->z),
-      .z = (a->x * b->y) - (a->y * b->x),
+      .x = (a.y * b.z) - (a.z * b.y),
+      .y = (a.z * b.x) - (a.x * b.z),
+      .z = (a.x * b.y) - (a.y * b.x),
   };
 }
