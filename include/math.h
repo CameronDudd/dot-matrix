ba802af615e1bbb27a98c4fa8e5ba74d34d759fe
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-09-29 21:02:41 +0100

    feat(math): add INF -INF

--- include/math.h
+++ include/math.h
@@ -6,6 +6,8 @@
 #ifndef MATH_H
 #define MATH_H
 
+#include <stdint.h>
+
 #define PI               (float)3.14159265359
 #define SQRT2            (float)1.41421356237
 #define SQRT3            (float)1.73205080757
@@ -15,6 +17,10 @@
 
 #define MIN(a, b) ((a < b) ? (a) : (b))
 #define MAX(a, b) ((a > b) ? (a) : (b))
+
+#define INF     (*(const float *)&INF_addr)
+#define NEG_INF (*(const float *)&NEG_INF_addr)
+extern const uint32_t INF_addr, NEG_INF_addr;
 
 float sin(float theta);
 float cos(float theta);
